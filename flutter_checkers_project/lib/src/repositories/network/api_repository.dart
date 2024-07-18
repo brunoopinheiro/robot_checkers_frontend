99381249f7416c572d69a3dfe2a89006f4fcbb9c
Laura Luna <lauraluna.siqueira@hotmail.com>
2024-07-18 09:31:00 -0300

    [REFACTOR] model root api

--- flutter_checkers_project/lib/src/repositories/network/api_repository.dart
+++ flutter_checkers_project/lib/src/repositories/network/api_repository.dart
@@ -1,9 +1,14 @@
 import 'package:http/http.dart' as http;
 
 Future<http.Response> fetchStart() {
-  return http.get(Uri.parse('https://flask/jogar'));
+  return http.get(Uri.parse('https://flask/start_game'));
+  
 }
 
 Future<http.Response> fetchState() {
-  return http.get(Uri.parse('https://flask/state'));
+  return http.post(Uri.parse('https://flask/update_board'));
+}
+
+Future<http.Response> fetchBoard() {
+  return http.post(Uri.parse('https://flask/robot_turn'));
 }
